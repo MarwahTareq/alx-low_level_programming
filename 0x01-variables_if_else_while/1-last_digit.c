69d1f86477294c20471a4c020dab013877633cb0
MarwahTareq <marwatarek55@gmail.com>
2023-03-18 01:33:17 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,5 @@ else if (m == 0)
 printf("last digit of %d is %d and is 0\n", n, m);
 else if (m < 6 && m != 0)
 printf("last digit of %d is %d and less than 6 and not 0\n", n, m);
-else
 return (0);
 }
