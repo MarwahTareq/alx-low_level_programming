1b6c1be48c3e8958ed81fe019d3b19cdb9967f5c
MarwahTareq <marwatarek55@gmail.com>
2023-04-17 17:18:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main: Entry Point
- *
- * return: 1 (success)
+ *main: Entry Point
+ *description: write ( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+ *return: Always 1 (success)
  */
 int main(void)
 {
-	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, x, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
