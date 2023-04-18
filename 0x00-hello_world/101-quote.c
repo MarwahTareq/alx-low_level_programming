de9fa6be7411530126ada0a7d5b1c0c272d08ada
MarwahTareq <marwatarek55@gmail.com>
2023-04-17 17:12:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  *
  * return: 1 (success)
  */
-int main (void)
+int main(void)
 {
 	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,x,59);
+	write(1, x, 59);
 	return (1);
 }
