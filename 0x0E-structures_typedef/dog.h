cf68acf47374ce8bd786c5fcf7c244209d5393b6
MarwahTareq <marwatarek55@gmail.com>
2023-04-28 14:58:10 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owener's name
+ *  Description: just a longdog structin a big kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
