3e6f4079243324f73906557e1a000dabfd87f4e2
MarwahTareq <marwatarek55@gmail.com>
2023-04-27 18:39:21 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,8 @@
-global    main
-extern    printf
+global main
+extern printf
 main:
-call  printf
-xor   eax, eax
-mov 	eax, 0
-mov   edi, format
-ret
-format: db `Hello, Holberton\n`,0
+mov edi, msg
+mov eax, 0
+call printf
+
+msg db 'Hello, Holberton', 0xa, 0
