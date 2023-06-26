1b043094a96d5a9f2f995a55bb020506cdeb56f6
MarwahTareq <marwatarek55@gmail.com>
2023-06-25 17:16:33 -0700

    anything file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program thet perform simple operation
+ * @argc: number of arguments
+ * @argv: array og arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(arg1, arg2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+/**
+ * op_add - calculate the sum of two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - calculate the diffrent of two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: diffrence of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - calculate the dividition of two integers
+ * @a: first integer
+ * @b: second integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - calculate the remainders
+ * @a: first integer
+ * @b: second integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculate the remainders
+ * @a: first integer
+ * @b: second integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
